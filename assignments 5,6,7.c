1fdc3d582b376f941bf0fe3794b325c6a68c3362
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2018-09-17 01:15:16 +0200

    added code for assignment 7

--- assignments 5,6,7.c
+++ assignments 5,6,7.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int example_array[] = {8000, -2, 1, 30, 12, -20, -99999};
 int array_size = 7;
-_Bool use_linked_list = 0;
+_Bool use_linked_list = 1;
 
 
 void print_array(int array[], int length) {
@@ -25,13 +26,60 @@ void array_separate(int array[], int length) { //separate negative ints using sw
   print_array(array, length);
 }
 
+struct element { //element for a doubly linked list
+  int value;
+  void* prev;
+  void* next;
+};
+
+void print_list(struct element *e) { //prints the contents of a list by iterating forwards until it finds an element pointing to NULL.
+  while(1) { //loop until NULL is hit
+    printf("%d, ", (*e).value); //print value of current element
+    if((*e).next == NULL) //break if element only points to null
+      break;
+    e = (*e).next; //move to next element
+  }
+  putchar('\n');
+}
+
 void list_separate(int array[], int length) { //separate negative ints using a linked list
+  //create first element in list
+  struct element *first = malloc(sizeof(struct element));
+  (*first).value = array[0];
+  (*first).prev = NULL;
+  (*first).next = NULL;
+  struct element *last = first;
+
+  print_list(first); //print initial list
+
+  int i;
+  for(i = 1; i < length; i++) {
+    //create new element for each int in array
+    struct element *e = malloc(sizeof(struct element));
+    (*e).value = array[i];
+
+    if(array[i] < 0) { //add element to front since it's negative
+      (*first).prev = e;
+      (*e).next = first;
+      (*e).prev = NULL;
+      first = e;
+    }
+
+    else { //add element to back since it's positive
+      (*last).next = e;
+      (*e).prev = last;
+      (*e).next = NULL;
+      last = e;
+    }
+
+    print_list(first);
+  }
+
   return;
 }
 
 int main(void) {
   print_array(example_array, array_size);
-  int* pointer = 0;
   if(!use_linked_list)
     array_separate(example_array, array_size);
   else
